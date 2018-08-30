0296f4087ef4efccf802ff61299a09aadc84a90d
Patrick Holec <pholec@mit.edu>
2018-08-30 18:01:08 -0400

    starting to build simulator and card piles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from cards.card import Card
 
+from utils.deck import CardPile
+
 import cards.relics.database as relic_db
 import cards.monsters.database as monster_db
 import cards.ironclad.database as ironclad_db
@@ -11,12 +13,18 @@ db['relics'] = relic_db.make()
 db['monsters'] = monster_db.make()
 db['ironclad'] = ironclad_db.make()
 
-print db['relics']
-print db['monsters']
-print db['ironclad']
+deck = CardPile(name = 'My Deck')
 
+strike = db['ironclad'].get_card_by_name('Strike')
+defend = db['ironclad'].get_card_by_name('Defend')
+bash   = db['ironclad'].get_card_by_name('Bash')
 
-db['ironclad'].render_all()
+deck.add_card(strike,copies = 5)
+deck.add_card(defend,copies = 4)
+deck.add_card(bash,copies = 1)
+
+deck.shuffle()
+
 
 
 
---
+++ utils/deck.py
@@ -0,0 +1,68 @@
+
+import random 
+import copy
+
+"""
+DECK OBJECT
+"""
+
+class CardPile(object):
+
+    def __init__(self,name = 'Deck'):
+        """ Initialize deck """
+        self.name = name
+        self.contents = []
+
+    def __iadd__(self,new_card):
+        """ Add a card to database """
+        self.contents += [result]
+
+    def add_card(self,new_card,copies = 1):
+        """ Add card to deck """
+        for _ in xrange(copies):
+            self.contents += [new_card]
+
+    def remove_card(self,card_index):
+        """ Remove card in deck by index """
+        self.cards.pop(card_index)
+
+    def __repr__(self):
+        display = 'Card Pile - {}'.format(self.name)
+        for index,card in enumerate(self.contents):
+            display += '\n{} : {}'.format(index+1,card.name)
+        return display
+
+    def shuffle(self):
+        random.shuffle(self.contents)
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+
+     
+'''
+my_deck = Deck()
+
+my_deck.add_card(strike_card,copies = 4)
+my_deck.add_card(block_card,copies = 5)
+my_deck.add_card(bash_card,copies = 1)
+
+my_deck.display()
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
