1b2af0e99c0f443c532bb010dcee30881bd25aa9
Pandyr <hole0077@umn.edu>
2018-08-30 20:07:28 -0400

    added card templates

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from cards.card import Card
 
 import cards.relics.database as relic_db
 import cards.monsters.database as monster_db
+import cards.ironclad.database as ironclad_db
 
 db = {}
 
-db['relics'] = relic_db.make()
-db['monsters'] = monster_db.make()
+#db['relics'] = relic_db.make()
+#db['monsters'] = monster_db.make()
+db['ironclad'] = ironclad_db.make()
 
-print db['relics']
-print db['monsters']
+#print db['relics']
+#print db['monsters']
+
+print db['ironclad']
 
